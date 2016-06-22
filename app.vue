bf371168d9cb66abfd56152038a4045a2d6e3721
Chris Griffith <chris@octopuscreative.com>
2016-06-22 12:13:39 -0700

    Update structure

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script lang="coffee">
+  
+  # Import Vue.
+  Vue = require 'vue'
+
+  # Instantiate Vue instance.
+  app = new Vue({
+    el: 'body'
+    components:
+      mailchimpSubscribe: require('./components/mailchimp-subscribe')
+  })
+
+</script>
