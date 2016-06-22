4ed91cb888dec1869fdd20355e077ce3046d6a03
Chris Griffith <chris@octopuscreative.com>
2016-06-22 12:38:47 -0700

    Add readme and product image gallery component

---
+++ README.md
@@ -0,0 +1,3 @@
+## Barebones Vue Components
+
+This is a collection of Vue.js components I've used in a number of projects, stripped of the project-specific markup to make them flexible, easy to use and customize in any project.
---
+++ components/product-image-gallery.vue
@@ -0,0 +1,49 @@
+<script lang="coffee">
+
+# Usage:
+# 
+#   <product-image-gallery
+#     :images=[
+#       '/images/image1.jpg',
+#       '/images/image2.jpg'
+#     ]
+#   ></product-image-gallery>
+
+module.exports =
+  name: "ProductImageGallery"
+
+  data: ->
+    activeImage: null
+
+  props:
+    images:
+      type: Array
+      required: true
+  
+  ready: ->
+    # When the component is ready, assign the active image as the first image
+    # in the images array.
+    @activeImage = @images[0]
+
+  methods:
+    selectImage: (image) ->
+      # Set the active image as the selected image.
+      @activeImage = image
+
+</script>
+
+<template>
+
+    <!-- Main/active image. -->
+    <div>
+      <img :src="activeImage" />
+    </div>
+
+    <!-- Thumbnails. -->
+    <div>
+      <div v-for="image in images" @click="selectImage(image)" :class="{ 'active': image == activeImage }">
+        <img :src="image" />
+      </div>
+    </div>
+
+</template>
